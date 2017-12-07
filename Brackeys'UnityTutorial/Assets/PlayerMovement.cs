564436a4e9a08273107f00878d9d57ab35f00e9a
TOC <thomash1924@gmail.com>
2017-12-07 06:55:18 +0000

    Added Player Movement Code
    Completed the second tutorial and added player movement code.

---
+++ Brackeys'UnityTutorial/Assets/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rd;
+
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        rd.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
